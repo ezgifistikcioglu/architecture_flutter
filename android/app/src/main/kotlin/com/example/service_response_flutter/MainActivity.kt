76e014aa5d7519b315f21f4e9b311185bedfb89a
Ezgi Fistikcioglu <fistikciogluezgi@gmail.com>
2021-08-26 22:42:41 +0300

    Completed application architecture.

---
+++ android/app/src/main/kotlin/com/example/service_response_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.acrhitecture_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/feature/login/login_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  setUp(() {
+    print("Here");
+  });
+
+  test("User Login Fail Test", () {
+    final isUserLogin = true;
+
+    expect(isUserLogin, isTrue);
+  });
+
+  group("User Login Full Test", () {
+    // MARK: test1
+    test("User Login Fail Test", () {
+      final isUserLogin = true;
+
+      expect(isUserLogin, isTrue);
+    });
+
+    // MARK: test2
+    test("User Login Fail Test", () {
+      final isUserLogin = true;
+
+      expect(isUserLogin, isTrue);
+    });
+
+    // MARK: test3
+    test("User Login Fail Test", () {
+      final isUserLogin = true;
+
+      expect(isUserLogin, isTrue);
+    });
+  });
+}
